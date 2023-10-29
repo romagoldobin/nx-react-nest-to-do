7d7879f3f012ca92d148f0b63d9aef9b4492482e
romagoldobin <goldobinforwork@gmail.com>
2023-10-30 00:35:52 +0500

    added db config

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from '../config/db-connect.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'env/backend.env',
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], 
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
